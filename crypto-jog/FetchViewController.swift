e93acaeccfa93b8d45bd87fd3fb0ad940e35bd3a
rachnera <rachnera@gmail.com>
2019-11-03 00:41:04 -0400

    Add Pod File

---
+++ crypto-jog/FetchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FetchViewController.swift
+//  crypto-jog
+//
+//  Created by Rachnera on 11/2/19.
+//  Copyright © 2019 Rachnera. All rights reserved.
+//
+
+import Foundation
---
+++ crypto-jog/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  crypto-jog
+//
+//  Created by Rachnera on 11/2/19.
+//  Copyright © 2019 Rachnera. All rights reserved.
+//
+
+import Foundation
